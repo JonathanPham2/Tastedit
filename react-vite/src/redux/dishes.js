a6085bd7652bdfb07bf27ee38451a65a1e1b8dfa
Thanh Pham <jonathanphamw22@gmail.com>
2024-04-25 14:48:45 -0700

    nothing

--- react-vite/src/redux/dishes.js
+++ react-vite/src/redux/dishes.js
@@ -4,7 +4,9 @@ const LOAD_DISHES = "/dishes"  // load all dishes
 
 const LOAD_DISH_BY_ID = "/dishes/id" // load dish by id
 
-const UPDATE_DISH = "/dish/update" // update dish
+const UPDATE_DISH = "/dish/update" // update dish post
+
+const DELETE_DISH = "dish/delete" // delete dish post
 
 
 
